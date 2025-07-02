bf06c7a4348a09749754740d7357bf44226f54cb
jerald-raphel <jjeraldraphel@gmail.com>
2025-07-02 10:40:39 +0530

    update routes models

--- routes/locationRoute.js
+++ routes/locationRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Form = require("../models/Form");
+const Form = require("../models/From");
 
 router.post("/submit", async (req, res) => {
   try {
